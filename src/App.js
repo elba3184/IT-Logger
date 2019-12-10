a7e4f73e4f24eb8135c9798b6e9cd210331250a2
Elba Chimilio <elbachimilio@gmail.com>
2019-12-10 17:44:24 -0500

    Created add-btn and modal for adding new logs using materialize

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Fragment, useEffect } from 'react';
 import SearchBar from './components/layout/SearchBar.js';
 import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn.js';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
+import AddLogModal from './components/logs/AddLogModal.js';
 
 const App = () => {
   useEffect(() => {
@@ -14,6 +16,8 @@ const App = () => {
     <Fragment>
       <SearchBar />
       <div className='container'>
+        <AddBtn />
+        <AddLogModal />
         <Logs />
       </div>
     </Fragment>
