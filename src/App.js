ad90e15958fa19e5caafee0ef57d00e2fd186a0d
Elba Chimilio <elbachimilio@gmail.com>
2019-12-10 17:12:10 -0500

    Finished prelimary UI for logs and added Preloader from materialize

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Fragment, useEffect } from 'react';
 import SearchBar from './components/layout/SearchBar.js';
-import Logs from './components/layout/logs/Logs.js';
+import Logs from './components/logs/Logs';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
