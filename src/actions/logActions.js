558d742cd3bc17c9a7d86eb15de6feaacebd19ae
Elba Chimilio <elbachimilio@gmail.com>
2019-12-16 17:59:20 -0500

    Delete logs connected with Redux

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,10 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from './types.js';
+import {
+  GET_LOGS,
+  SET_LOADING,
+  LOGS_ERROR,
+  ADD_LOG,
+  DELETE_LOG,
+} from './types.js';
 
 //Get logs from server
 export const getLogs = () => async (dispatch) => {
@@ -42,6 +48,27 @@ export const addLog = (log) => async (dispatch) => {
     dispatch({
       type: LOGS_ERROR,
       payload: err.response.data,
+    });
+  }
+};
+
+//Delte log from server
+export const deleteLog = (id) => async (dispatch) => {
+  try {
+    setLoading();
+
+    await fetch(`/logs/${id}`, {
+      method: 'DELETE',
+    });
+
+    dispatch({
+      type: DELETE_LOG,
+      payload: id,
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.statusText,
     });
   }
 };
