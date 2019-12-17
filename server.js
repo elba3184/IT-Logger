439b1d15384e1f05a1ab4668426a88ff808c2c53
Elba Chimilio <elbachimilio@gmail.com>
2019-12-16 20:07:21 -0500

    Trying to deploy to Heroku

--- server.js
+++ server.js
@@ -1,26 +1,10 @@
-const express = require('express');
-const path = require('path');
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
 
-const app = express();
+server.use(middlewares);
+server.use(router);
 
-//Connect Database
-connectDB();
-
-//Init Middleware - replaces body-parser
-app.use(express.json({ extended: false }));
-
-//Server static assets in production
-if (process.env.NODE_ENV === 'production') {
-  //Set static folder
-  app.use(express.static('/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-  });
-}
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
+server.listen(port);
