1839676ad837e46c0c077e2b545e276aec39075d
Elba Chimilio <elbachimilio@gmail.com>
2020-06-30 09:34:07 -0400

    Updates

---
+++ precache-manifest.fd1d77cce5a41362e857ac25fc7f44ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1636b366c6192cdefcbb08cb3fc94a0e",
+    "url": "/it-logger-app/index.html"
+  },
+  {
+    "revision": "70b9a575f3683c615564",
+    "url": "/it-logger-app/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "2e0122cdd15d9d7cc7a5",
+    "url": "/it-logger-app/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "70b9a575f3683c615564",
+    "url": "/it-logger-app/static/js/2.d2355d36.chunk.js"
+  },
+  {
+    "revision": "091cc4ef4788c447793b28792926e07e",
+    "url": "/it-logger-app/static/js/2.d2355d36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e0122cdd15d9d7cc7a5",
+    "url": "/it-logger-app/static/js/main.3dee5ff4.chunk.js"
+  },
+  {
+    "revision": "9ed1ab3757470c55107a",
+    "url": "/it-logger-app/static/js/runtime-main.c336f839.js"
+  }
+]);
